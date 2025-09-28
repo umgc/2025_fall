96971a691b2eef48d350161f2e167e4fe6a03987
Ryan Appleby <rapplebyj@gmail.com>
2025-09-28 16:59:08 -0400

    redo git

--- LearningLens2025/frontend/android/app/src/main/kotlin/com/example/learninglens_app/MainActivity.kt
+++
@@ -1,5 +0,0 @@
-package com.example.learninglens_app
-
-import io.flutter.embedding.android.FlutterActivity
-
-class MainActivity : FlutterActivity()
--- LearningLens2025/frontend/lib/Api/lms/enum/preview_version_enum.dart
+++
@@ -1,2 +0,0 @@
-//Enum for PreviewVersion
-enum PreviewVersion { PREVIEW_VERSION_UNSPECIFIED, PUBLISHED_VERSION }
