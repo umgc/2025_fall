e31dd33ca49d074c098804abd17822f3ac77fce8
William <wfreem2@gmail.com>
2025-09-29 17:29:28 -0400

    Added test script for running. Started terraform edits.

--- LearningLens2025/terraform/main.tf
+++ LearningLens2025/terraform/main.tf
@@ -341,3 +341,71 @@ resource "aws_lambda_function_url" "get_token_url" {
     allow_headers = ["content-type"]
   }
 }
+
+
+# S3 bucket
+resource "aws_s3_bucket" "edulense" {
+  bucket = "edulense"
+}
+
+# IAM Policy for S3 read/write access
+resource "aws_iam_policy" "edulense_rw" {
+  name        = "EdulenseS3AccessPolicy"
+  description = "Allow read/write access to the edulense bucket"
+
+  policy = jsonencode({
+    Version = "2012-10-17"
+    Statement = [
+      {
+        Effect = "Allow"
+        Action = [
+          "s3:ListBucket"
+        ]
+        Resource = [
+          "arn:aws:s3:::edulense"
+        ]
+      },
+      {
+        Effect = "Allow"
+        Action = [
+          "s3:GetObject",
+          "s3:PutObject",
+          "s3:DeleteObject"
+        ]
+        Resource = [
+          "arn:aws:s3:::edulense/*"
+        ]
+      }
+    ]
+  })
+}
+
+# IAM Role for ECS Tasks
+resource "aws_iam_role" "code_evaluator" {
+  name = "CodeEvaluator"
+
+  assume_role_policy = jsonencode({
+    Version = "2012-10-17"
+    Statement = [
+      {
+        Effect = "Allow"
+        Principal = {
+          Service = "ecs-tasks.amazonaws.com"
+        }
+        Action = "sts:AssumeRole"
+      }
+    ]
+  })
+}
+
+# Attach custom policy
+resource "aws_iam_role_policy_attachment" "attach_custom" {
+  role       = aws_iam_role.code_evaluator.name
+  policy_arn = aws_iam_policy.edulense_rw.arn
+}
+
+# Attach AWS managed ECS Task Execution policy
+resource "aws_iam_role_policy_attachment" "attach_ecs_managed" {
+  role       = aws_iam_role.code_evaluator.name
+  policy_arn = "arn:aws:iam::aws:policy/service-role/AmazonECSTaskExecutionRolePolicy"
+}
