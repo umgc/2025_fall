6c67e9d0822ef71f21ab396c6b83217911e18fc8
Avian Overlord <wrave@gwu.edu>
2025-10-05 20:11:12 -0400

    More dummy code for the CheckInController.

--- careconnect2025/backend/core/src/main/java/com/careconnect/controller/CheckInController.java
+++ careconnect2025/backend/core/src/main/java/com/careconnect/controller/CheckInController.java
@@ -9,30 +9,37 @@ import java.util.List;
 
 @RestController
 @RequestMapping("/v1/checkins")
-@Tag(name = "Check-In", description = "Endpoint for the virtual Check-In, including both patient submitting and ")
+@Tag(name = "Check-In", description = "Endpoint for the virtual Check-In, including both patient submitting and caregiver checking")
 public class CheckInController {
 
     @PostMapping()
     public ResponseEntity<CheckIn> patientCheckIn()
     {
+        //This function creates patient check-ins. Presumably by accessing the current data from the screen.
+        //TODO:Check how that works
         return  new ResponseEntity<CheckIn>();
     }
 
     @GetMapping()
     public List<ResponseEntity<CheckIn>> getCheckIns()
     {
+        //This function lists patient check-ins that meet a criteria
         return new List<ResponseEntity<CheckIn>>[];
     }
 
     @GetMapping("/{id}")
-    public CheckIn getCheckIn()
+    public CheckIn getCheckIn(@PathVariable Long id)
     {
-        return new CheckIn();
+        //This function retrieves a specific check-in by ID
+        CheckIn target = CheckInService.getCheckInByID(id);
+        //Validate access to checkIn, perform checks
+        return target;
     }
 
     @PutMapping("/{id}")
     public  ResponseEntity<CheckIn> updateCheckIn()
     {
+        //This function updates a specific check-in by ID
         return new ResponseEntity<CheckIn>();
     }
 }
