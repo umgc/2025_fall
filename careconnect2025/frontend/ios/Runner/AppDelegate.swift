18866f759f06eccd2d2a4b00fab1eefa98602578
Alondra M. Vega Pacheco <167393570+habichuela13@users.noreply.github.com>
2025-10-23 17:13:13 -0500

    app delegate reverted backto how it was

--- careconnect2025/frontend/ios/Runner/AppDelegate.swift
+++ careconnect2025/frontend/ios/Runner/AppDelegate.swift
@@ -8,9 +8,8 @@ import UIKit
     didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?
   ) -> Bool {
     GeneratedPluginRegistrant.register(with: self)
-    // TODO: Add OcrHandler back when needed
-    // let controller = window?.rootViewController as! FlutterViewController
-    // OcrHandler.register(with: controller.binaryMessenger)
+    let controller = window?.rootViewController as! FlutterViewController
+    OcrHandler.register(with: controller.binaryMessenger)
     return super.application(application, didFinishLaunchingWithOptions: launchOptions)
   }
 }
