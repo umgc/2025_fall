c41c5ddfa318ece431526edd55b30acd646267f5
isabel-santiagolewis <isabel.santiagolewis@gmail.com>
2025-10-22 21:47:03 -0400

    Added Alexa Frontend

--- careconnect2025/frontend/lib/config/router/app_router.dart
+++ careconnect2025/frontend/lib/config/router/app_router.dart
@@ -61,7 +61,7 @@ import 'package:care_connect_app/features/invoices/screens/dashboard/invoice_das
 import 'package:care_connect_app/features/invoices/screens/invoice_detail_page.dart';
 import 'package:care_connect_app/features/invoices/screens/invoice_list_page.dart';
 import 'package:care_connect_app/features/invoices/models/invoice_models.dart';
-
+import 'package:care_connect_app/features/auth/presentation/pages/AlexaLoginPage.dart';
 
 /// Helper function to navigate to the appropriate dashboard based on stored user role
 Future<void> navigateToDashboard(BuildContext context, {int? tabIndex}) async {
@@ -683,7 +683,11 @@ final GoRouter appRouter = GoRouter(
       path: '/virtual-checkin',
       builder: (context, state) => const PatientVirtualCheckIn(),
     ),
-    
+        //Adding Calendar Assistant route
+    GoRoute(
+      path: '/alexaLogin',
+      builder: (_, __) => const AlexaLoginPage(),
+    ),
 
     // Handle routes from legacy menus
     GoRoute(
