3d9e1d896b0bcb2a46f994ee2a39e7ab8cea4675
phans09 <sadina.wagner@gmail.com>
2025-10-23 21:29:40 -0400

    Create AslServiceApp as Spring Boot application

---
+++ careconnect2025/backend/swiftasl-service/src/main/java/com/careconnect/asl/AslServiceApp.java
@@ -0,0 +1,10 @@
+package com.careconnect.asl;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AslServiceApp {
+  public static void main(String[] args) {
+    SpringApplication.run(AslServiceApp.class, args);
+  }
+}
