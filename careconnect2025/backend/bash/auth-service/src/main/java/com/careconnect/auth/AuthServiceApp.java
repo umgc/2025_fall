f6ca0d0fe0f2022ae00d5a5f3d288a0458347c0f
phans09 <sadina.wagner@gmail.com>
2025-10-23 22:41:05 -0400

    Add AuthServiceApp as Spring Boot application

---
+++ careconnect2025/backend/bash/auth-service/src/main/java/com/careconnect/auth/AuthServiceApp.java
@@ -0,0 +1,8 @@
+package com.careconnect.auth;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AuthServiceApp {
+  public static void main(String[] args) { SpringApplication.run(AuthServiceApp.class, args); }
+}
